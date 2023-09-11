65a7657224ce625efb177f1a244c974bd52ea275
isybliss <isyblissumunna@gmail.com>
2023-09-11 07:59:17 +0100

    Updated the validation

--- frontend_section/src/Signup.js
+++ frontend_section/src/Signup.js
@@ -1,89 +1,69 @@
 import React, {useState} from 'react'; 
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom'
+import Validation from './SignupValidation';
 import './Signup_login.css';
 import { toast } from 'react-toastify';
 
 function Signup() {
 
-    const [id, idchange] = useState("");
-    const [email, emailchange] = useState("");
-    const [password, passwordchange] = useState("");
-
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        password: ''
+    }) 
     const navigate = useNavigate();
-
-    const isValidate = () => {
-        let isproceed = true;
-        let errormessage = 'Please enter the value in ';
-        if (id === null || id === "") {
-            isproceed = false;
-            errormessage += 'Username';
-        }
-        if (email === null || email === "") {
-            isproceed = false;
-            errormessage += ' Email';
-        }
-        if (password === null || password === "") {
-            isproceed = false;
-            errormessage += ' Password';
-        }
-        if(!isproceed) {
-            toast.warning(errormessage);
-        }else {
-            if (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/.test(password)) {
-
-            }else {
-                isproceed = false;
-                toast.warning("Password shouls be atleast 8 characters including uppercase, lowercase and numbers")
-            }
-        }
-        return isproceed;
-    }    
-    const handleSubmit =(e) => {
-        
-        e.preventDefault();
-        let regobj = { id, email, password };
-        if(isValidate()) {
+    const [errors, setErrors] = useState({})
+    const handleInput = (event) => {
+         setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
+    }
+    const handleSubmit =(event) => {
+        event.preventDefault();
+        const err = Validation(values);
+        setErrors(err);
+        if(err.name === "" && err.email === "" && err.password === "") {
             fetch("http://localhost:8000/user", {
-                method:"POST",
-                headers:{ "content-type":"application/json" },
-                body:JSON.stringify(regobj)
+                method: "POST",
+                headers:{ "content-type" : "application/json" },
+                body:JSON.stringify(values)
             }).then((res) => {
-                toast.success('Registered Successfully');
-                navigate('/')
-            }).catch((err)=>(
-                toast.error('Failed :'+err.message)
+                toast.success("Registered Successfully");
+                navigate('/');
+            })
+            .catch((err) => (
+                toast.error("Failed :"+err.message)
             ));
         }
-    }
+    } 
+
     return (
-        <div className='d-flex justify-content-center align-items-center vh-100 bg-color'>
-            <div className='rounded container-1'>
+        <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
+            <div className='bg-white p-3 rounded w-50 container'>
                 <div className="header">
-                    <div className="text">Register</div>
+                    <div className="text">Sign In</div>
                     <div className="underline"></div>
                 </div>
-                <form onSubmit={handleSubmit}>
-                    <div className='inputs'>
-                        <label htmlFor='name'><strong>Username</strong><span className='errmsg'>*</span></label>
+                <form action="" onSubmit={handleSubmit}>
+                    <div className='mb-3'>
+                        <label htmlFor='name'><strong>Username</strong></label>
                         <input type='text' placeholder='Enter Username' name='name'
-                        value={id} onChange={e=>idchange(e.target.value)} className='form-control' />
+                        onChange={handleInput} className='form-control' />
+                        {errors.name && <span className='text-danger'> {errors.name}</span>}
                     </div>
-                    <div className='inputs'>
-                        <label htmlFor='email'><strong>Email</strong><span className='errmsg'>*</span></label>
+                    <div className='mb-3'>
+                        <label htmlFor='email'><strong>Email</strong></label>
                         <input type='email' placeholder='Enter Email' name='email'
-                        value={email} onChange={e=>emailchange(e.target.value)} className='form-control' />
-                       
+                        onChange={handleInput} className='form-control' />
+                        {errors.email && <span className='text-danger'> {errors.email}</span>}
                     </div>
-                    <div className='inputs'>
-                        <label htmlFor='password'><strong>Password</strong><span className='errmsg'>*</span></label>
+                    <div className='mb-3'>
+                        <label htmlFor='password'><strong>Password</strong></label>
                         <input type='password' placeholder='Enter Password' name='password'
-                        value={password} onChange={e=>passwordchange(e.target.value)} className='form-control' />
-                        
+                        onChange={handleInput} className='form-control' />
+                        {errors.password && <span className='text-danger'> {errors.password}</span>}
                     </div>
                     <button type='submit' className='btn-success rounded submit'>Sign up</button>
-                    <p className='forgot-password'>Already have an account ?</p>
-                    <Link to="/" className='border bg-light rounded login'>Login</Link>
+                    <p className='forgot-password'>Already have an account</p>
+                    <Link to="/" className='btn btn-default border w-100 bg-light rounded text-decoration-none'>Login</Link>
                 </form>
             </div>
         </div>
--- frontend_section/src/SignupValidation.js
+++ frontend_section/src/SignupValidation.js
@@ -1,17 +1,4 @@
-import { toast } from 'react-toastify';
-
-function validation(id, email, password) {
-    let isproceed = true;
-    let errormessage = 'Please enter the value in ';
-    if (id === null || id === "") {
-        isproceed = false;
-        errormessage += 'Username';
-    }
-    if(!isproceed) {
-        toast.warning(errormessage)
-    }
-    return isproceed;
-    /*
+function validation(values) {
     let error = {}
     const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
     const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
@@ -36,12 +23,11 @@ function validation(id, email, password) {
         error.password = "Input password"
     }
     else if(!password_pattern.test(values.password)) {
-        error.password = "Password must be atleast 8 characters including capital letters and numbers"
+        error.password = "Password should be atleast 8 characters including uppercase, lowercase and numbers"
     }else {
         error.password = ""
     }
     return error
-    */
 }
 
 export default validation;
