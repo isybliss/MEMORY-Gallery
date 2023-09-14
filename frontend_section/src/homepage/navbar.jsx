31e626295bdb7a65b9b50ca7357d0d58bea836b9
isybliss <isyblissumunna@gmail.com>
2023-09-14 22:54:19 +0100

    Removed the search form and button from navbar

--- frontend_section/src/homepage/navbar.jsx
+++ frontend_section/src/homepage/navbar.jsx
@@ -33,17 +33,6 @@ const Navbar = () => {
             </a>
           </li>
         </ul>
-        <form className="d-flex">
-          <input
-            className="form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button className="btn btn-outline-success" type="submit">
-            Search
-          </button>
-        </form>
         <div>
             {/* <span className='ms-2'>Account</span> */}
             <span className='ms-2'>Sign-up</span>
